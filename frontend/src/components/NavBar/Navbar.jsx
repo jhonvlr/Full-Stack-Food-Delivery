fbbfde52316447f02f90f4e87b70ef11fc5ae176
jhonvlr <jhonzincwb777@hotmail.com>
2024-11-29 09:56:54 -0300

    construindo a lista de pedidos

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = ({ setShowLogin }) => {
           home
         </Link>
         <a
-          href="explore-menu"
+          href="#explore-menu"
           onClick={() => setMenu("menu")}
           className={menu === "menu" ? "active" : ""}
         >
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -19,6 +19,13 @@ const StoreContextProvider = (props) => {
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
   };
 
   const contextValue = {
@@ -27,6 +34,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount,
   };
   return (
     <StoreContext.Provider value={contextValue}>
